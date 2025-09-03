cc1c093dd04cf48b874ef16ffdf08e537796e334
Henrich Horváth <henrich.horvath@portnox.com>
2025-09-03 17:21:08 +0200

    fix

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const { env } = require('node:process')
-// const { App } = require('@slack/bolt')
 
 const ALLOWED_USER = env.ALLOWED_USER;
 const JIRA_BASE = env.JIRA_BASE;
@@ -9,7 +8,7 @@ const JIRA_USER = env.JIRA_USER;
 const JIRA_ISSUE = env.JIRA_ISSUE;
 const JIRA_TOKEN = env.JIRA_TOKEN;
 
-const createWorklog = (issue, text, seconds) => {
+const createWorklog = (issue, text, timeSpent) => {
   const started = new Date().toISOString().replace("Z", "+0000");
 
   const bodyData = `{
@@ -29,7 +28,7 @@ const createWorklog = (issue, text, seconds) => {
     "version": 1
   },
   "started": "${started}",
-  "timeSpentSeconds": ${seconds}
+  "timeSpent": ${timeSpent}
   }`;
   console.log(bodyData);
 
@@ -50,48 +49,22 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true })); // Slack sends x-www-form-urlencoded
 
 app.post("/slack/log", async (req, res) => {
-  console.log(req.body);
+  const { user_id, text } = req.body;
 
-  const { user_id, issue, text, seconds } = req.body; 
-  const issueKey = issue ?? JIRA_ISSUE;
-  const contentText = text?.trim() ?? "";  
+  const textParts = text.split(" ");
+  const timeSpent = textParts[0];
+  const contentText = textParts[1] ?? "";  
+  const issue = textParts[2] ?? JIRA_ISSUE;
 
   if (user_id !== ALLOWED_USER) {
     return res.status(401).json({ text: "🚫 You are not allowed to use this command." });
   }
 
-  const result = await createWorklog(issueKey, contentText, seconds);
+  const result = await createWorklog(issue, contentText, timeSpent);
   
   console.log(`Status: ${result.status}`);
   console.log(await result.json());
 
   res.status(result.status).json();
 });  
-app.listen(8080, () => console.log("Listening on port 3000"));
-
-// const app = new App({
-//   token: SLACK_BOT_TOKEN,
-//   signingSecret: SLACK_SIGNING_SECRET,
-// });
-
-// // Slash command handler
-// app.command("/log", async ({ command, ack, respond }) => {
-//   console.log(command);
-
-//   await ack();
-
-//   const issue = command.issue?.trim() ?? JIRA_ISSUE;
-//   const text = command.text?.trim() ?? "";
-//   const seconds = parseInt(command.seconds.trim());
-  
-//   const result = await createWorklog(issue, text, seconds);
-  
-//   console.log(`Status: ${result.status}`);
-//   console.log(await result.json());
-
-//   await respond(result.status === 201 ? "✅" : "Error");
-// });
-
-// (async () => {
-//   await app.start(process.env.PORT || 3000);
-// })();
+app.listen(8080, () => console.log("Listening on port 8080"));
